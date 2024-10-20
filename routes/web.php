0445875a467fb88e5b4ed62d161a655c32d5a49f
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-10-20 15:00:57 +0300

    Add index todo route

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,13 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Todo\IndexController as TodoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/app', [TodoController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
