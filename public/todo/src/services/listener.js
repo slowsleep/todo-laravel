dac81824243c9b156a2591268047f0cf5e4a8cd7
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-10-21 22:22:36 +0300

    Edit listener func loginForm

--- public/todo/src/services/listener.js
+++ public/todo/src/services/listener.js
@@ -8,18 +8,17 @@ import { addTaskToList, renderCount } from "./render";
 export function loginForm() {
     const loginForm = document.querySelector("#app-login-form");
 
-    loginForm.addEventListener("submit", function (e) {
+    loginForm.addEventListener("submit", async function (e) {
         e.preventDefault();
         const formData = new FormData(loginForm);
-        const login = formData.get("login");
-        const password = formData.get("password");
-        const isAuth = authUser(login, password);
+        const isAuth = await authUser(formData);
 
         if (isAuth) {
             localStorage.setItem(
                 "user",
                 JSON.stringify({
                     id: appState.currentUser.id,
+                    name: appState.currentUser.name,
                     role: appState.currentUser.role,
                 })
             );
