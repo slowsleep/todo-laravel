b665d6ec891005659c9ab0714e2936ddd4766ec1
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-10-21 22:13:12 +0300

    Edit router in js and links

--- public/todo/src/services/router.js
+++ public/todo/src/services/router.js
@@ -1,32 +1,18 @@
 import * as render from "./render";
 
 export function route(appState) {
-    let curPath;
     let path = window.location.pathname;
-    let arrPath = path.split("/");
 
-    if (arrPath.length) {
-        curPath = arrPath[arrPath.length - 1]
-    }
-
-    // for github
-    let search = window.location.search;
-
-    if (search && search.startsWith("?p=")) {
-        curPath = search.replace("?p=/", "");
-    }
-
-    switch (curPath) {
-        case "":
+    switch (path) {
+        case "/app":
             return render.content(appState);
-        case "profile":
+        case "/app/profile":
             return render.profile(appState);
-        case "users":
+        case "/app/users":
             if (appState.currentUser.role == "admin") {
                 return render.adminUsers(appState);
             }
         default:
             return render.notFound();
     }
-
 }
