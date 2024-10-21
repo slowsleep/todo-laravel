a0d0c1059d5b7bff136123219135b4834412531d
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-10-21 22:21:35 +0300

    Edit authUser func to using laravel api via fetch

--- public/todo/src/services/auth.js
+++ public/todo/src/services/auth.js
@@ -1,10 +1,23 @@
 import { appState } from "../app";
 import { isExists, getUserById } from "../controllers/UserController";
 
-export const authUser = function (login, password) {
-    const userId = isExists(login, password);
-    if (!userId) return false;
-    const currentUser = getUserById(userId);
-    appState.currentUser = currentUser;
+export const authUser = async function (form) {
+    let response = await fetch('/api/login', {
+        method: 'POST',
+        headers: {
+            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+        },
+        body: form
+    }).then(response => {
+        return response;
+    });
+
+    const result = await response.json();
+
+    if (result.error) {
+        return false;
+    }
+
+    appState.currentUser = result.user;
     return true;
 };
