a6ad955d1d9bcee8fc461c4533743d94503aef5e
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-10-29 00:41:33 +0300

    Add logoutUser func js service to request api/logout

--- public/todo/src/services/auth.js
+++ public/todo/src/services/auth.js
@@ -21,3 +21,24 @@ export const authUser = async function (form) {
     appState.currentUser = result.user;
     return true;
 };
+
+export const logoutUser = async function () {
+    let response = await fetch('/api/logout', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+        },
+    }).then(response => {
+        return response;
+    });
+
+    const result = await response.json();
+
+    if (result.error) {
+        return false;
+    }
+
+    appState.currentUser = null;
+    return true;
+}
