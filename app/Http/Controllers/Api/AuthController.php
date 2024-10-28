f4c0cfd8e86284bb604ef05529a2574ed47b59bd
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-10-29 00:38:55 +0300

    Add logout, user methods to auth api controller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -23,4 +23,22 @@ class AuthController extends Controller
 
         return response()->json(['error' => true, 'error_message' => 'User not found'], 200);
     }
+
+    public function logout(Request $request)
+    {
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return response()->json(['error' => false, 'message' => 'User is logout'], 200);
+    }
+
+    public function user(Request $request)
+    {
+        if (Auth::check()) {
+            return response()->json(Auth::user());
+        }
+
+        return response()->json(null, 401);
+    }
 }
