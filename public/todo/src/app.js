1d81c650550aa952c179c0a2d0f70286cf37fef6
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-10-29 00:46:09 +0300

    Edit: using localstorage changed to use api. Edit links logic

--- public/todo/src/app.js
+++ public/todo/src/app.js
@@ -10,38 +10,56 @@ import { getFromStorage } from "./utils";
 import { route } from "./services/router";
 
 export const appState = new State();
-generateTestUser(User);
-let user = getFromStorage("user");
 
-if (user.length !== 0) {
-    appState.currentUser = user;
-    let isAdmin = user.role == "admin" ? true : false;
-    render.baseTemplate(appState, isAdmin);
-    route(appState);
-    listener.logout();
-} else {
-    render.navRight(false);
-    listener.loginForm();
+async function checkUserAuth() {
+    const response = await fetch('/api/auth/user', {
+        method: 'GET',
+    });
+
+    if (response.status === 401) {
+        return null;
+    }
+
+    const result = await response.json();
+    return result;
 }
 
-let links = document.querySelectorAll("a[href]");
-links.forEach((link) => {
-    link.addEventListener("click", function(e) {
-        e.preventDefault();
+async function initApp() {
 
-        let href = this.getAttribute('href');
-        let url = new URL(window.location.href);
-        let spltPathname = url.pathname.split("/")
-        let newEndPartPathname = href !== "/" ? href : "";
+    let user = await checkUserAuth();
 
-        if (spltPathname.length == 2) {
-            spltPathname[1] = newEndPartPathname;
-        } else if (spltPathname.length == 3) {
-            spltPathname[2] = newEndPartPathname;
-        }
+    if (user) {
+        appState.currentUser = user;
+        let isAdmin = user.role == "admin" ? true : false;
+        render.baseTemplate(appState, isAdmin);
+        route(appState);
+        listener.logout();
+    } else {
+        render.navRight(false);
+        listener.loginForm();
+    }
 
-        url.pathname = spltPathname.join("");
-        // write new href without source for github pages
-        window.location.href = url.origin + url.pathname;
+    setupLinkNavigation();
+}
+
+function setupLinkNavigation() {
+    let links = document.querySelectorAll("a[href]");
+    links.forEach((link) => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            let href = this.getAttribute('href');
+            let url = new URL(window.location.href);
+            let newUrl = url.origin + href;
+
+            window.history.pushState({}, "", newUrl)
+            route(appState);
+        });
     });
-});
+    window.addEventListener('popstate', () => {
+        route(appState);
+    });
+
+}
+
+initApp();
